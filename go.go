f57327ffd0114fca5100f73c655661b7e7c87abe
Joao Victor de Sousa Lima <joaovictordesousalima@192.168.0.24>
2025-12-06 00:05:05 -0300

    third i think

--- go.go
+++ go.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+type Carro struct {
+	marca  string
+	modelo string
+}
+
+var carro1 = Carro{"Porche", "911 carrera"}
+
 func main() {
-	fmt.Println("teste")
+	fmt.Println(carro1.marca)
 }

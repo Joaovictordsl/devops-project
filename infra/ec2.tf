ebc2e2342f20b30b8add0a4fa9894275e1923c68
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-01-22 03:43:24 -0300

    terra

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -71,7 +71,7 @@ resource "aws_security_group" "security_group" {
 
 resource "aws_instance" "website-server" {
   ami                    = "ami-07ff62358b87c7116"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = aws_key_pair.key_pair.key_name
   vpc_security_group_ids = [aws_security_group.security_group.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name

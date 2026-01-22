0f6fc59b4b1a06c6c55f37327a54797f837bc352
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-01-22 04:04:25 -0300

    dockerpermission

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -5,7 +5,7 @@ sudo yum update -y
 yum install -y docker
 systemctl start docker
 systemctl enable docker
-usermod -a -G ec2-user
+usermod -a -G docker ec2-user
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 yum install -y unzip

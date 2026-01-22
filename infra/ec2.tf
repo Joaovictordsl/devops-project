4a72fad730821305b8625ac20c4afecc607046d9
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-01-22 03:33:23 -0300

    iamrole

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -74,7 +74,7 @@ resource "aws_instance" "website-server" {
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.key_pair.key_name
   vpc_security_group_ids = [aws_security_group.security_group.id]
-
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   tags = {
     Name        = "website-server"
     Provisioned = "Terraform"

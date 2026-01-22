a7cae32cd0a81c1bc2232e115388d496739b1831
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-01-22 02:40:00 -0300

    tf_format

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,28 +1,28 @@
 resource "tls_private_key" "rsa_key" {
 
-   algorithm = "RSA"
-   rsa_bits = 4096
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "key_pair" {
-  key_name = "my-key-terraform"
+  key_name   = "my-key-terraform"
   public_key = tls_private_key.rsa_key.public_key_oppenssh
 
 }
 
-resource "local_file" "private_key"{
-  content = tls_private_key.rsa_key.private_key_pem
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_key.private_key_pem
   filename = "my-key-terraform.pem"
 }
 
 
-resource "aws_instance" "website-server"{
-  ami = "ami-07ff62358b87c7116"
+resource "aws_instance" "website-server" {
+  ami           = "ami-07ff62358b87c7116"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.key_pair.key_name
+  key_name      = aws_key_pair.key_pair.key_name
 
   tags = {
-    Name = "website-server"
+    Name        = "website-server"
     Provisioned = "Terraform"
   }
 
